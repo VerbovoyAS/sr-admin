31b5df1a4ceb60d249dde1f45771009dfd00d7ec
VerbovoyAS <verbovoyas@mail.ru>
2021-03-20 00:25:11 +0000

    sr-admin

--- www/test_db.php
+++ www/test_db.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("database", "root", $_ENV['MYSQL_ROOT_PASSWORD'], null);
+$link = mysqli_connect("database", "root", 'A_dmin_163', null);
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
@@ -11,3 +11,5 @@ if (!$link) {
 echo "Success: A proper connection to MySQL was made! The docker database is great." . PHP_EOL;
 
 mysqli_close($link);
+
+phpinfo();
